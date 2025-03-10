c025ff7f3c80d2b1c002583c4a98a78c35cedcff
Om Mishra <ommishra.work@gmail.com>
2025-03-10 11:07:14 +0530

    Added ShortenedURL model with 0001 migration file

--- gulliver_tunnel/shortener/models.py
+++ gulliver_tunnel/shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class ShortenedURL(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
