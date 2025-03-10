cdfaceaf0a56bef48c3611c01b2f7686aecaba3d
Om Mishra <ommishra.work@gmail.com>
2025-03-10 11:06:34 +0530

    Added User and OTP model with 0001 migration file

---
+++ gulliver_tunnel/user/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import uuid
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True, db_index=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self):
+        from datetime import timedelta, timezone
+        return self.created_at >= timezone.now() - timedelta(minutes=5)
