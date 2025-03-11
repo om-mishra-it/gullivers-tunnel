e7d9b933af9a88dd1eff72ef71139c2fb9ae5660
Om Mishra <ommishra.work@gmail.com>
2025-03-11 16:15:24 +0530

    Setup Token based authentication in Django settings

--- gulliver_tunnel/gulliver_tunnel/settings.py
+++ gulliver_tunnel/gulliver_tunnel/settings.py
@@ -139,3 +139,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
