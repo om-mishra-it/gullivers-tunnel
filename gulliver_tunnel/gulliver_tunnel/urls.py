c3c2b53a4b2b88be07722c3f3b90ae02939c96b3
Om Mishra <ommishra.work@gmail.com>
2025-03-11 16:25:50 +0530

    Integrated redirection to urls

--- gulliver_tunnel/gulliver_tunnel/urls.py
+++ gulliver_tunnel/gulliver_tunnel/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from user.viewsets import UserViewSet
-from shortener.viewsets import ShortenedURLViewSet
+from shortener.viewsets import ShortenedURLViewSet, RedirectView
 
 router = DefaultRouter()
 router.register(r'userviewset', UserViewSet, basename='userviewset')
@@ -11,5 +11,6 @@ router.register(r'shortenedurlviewset', ShortenedURLViewSet, basename='shortened
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("<str:short_code>/", RedirectView.as_view(), name="redirect"),
     path('api/v1/', include(router.urls)),
 ]
