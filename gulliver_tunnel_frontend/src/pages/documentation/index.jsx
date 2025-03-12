768cfec13685084efdf5fa7fdec5bd9a7c4837cc
Om Mishra <ommishra.work@gmail.com>
2025-03-12 15:41:23 +0530

    Added Documentation page

---
+++ gulliver_tunnel_frontend/src/pages/documentation/index.jsx
@@ -0,0 +1,12 @@
+export default function Documentation() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+            <h1 className="text-5xl font-bold text-gray-800 dark:text-gray-600">
+                Documentation
+            </h1>
+            <p className="mt-4 text-lg text-gray-600 dark:text-gray-500 max-w-2xl">
+                Docs.
+            </p>
+        </div>
+    );
+}
