9eb7408985e136ba7999d4c7ec84009a26e42ee7
Om Mishra <ommishra.work@gmail.com>
2025-03-12 12:00:32 +0530

    Registered api_access into urls

--- gulliver_tunnel/gulliver_tunnel/urls.py
+++ gulliver_tunnel/gulliver_tunnel/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from user.viewsets import UserViewSet
+from user.viewsets import UserViewSet, LoginViewSet, LogoutViewSet
 from shortener.viewsets import ShortenedURLViewSet, RedirectView
+from api_access.viewsets import APIKeyViewSet
 
 router = DefaultRouter()
 router.register(r'userviewset', UserViewSet, basename='userviewset')
+router.register(r'loginviewset', LoginViewSet, basename='loginviewset')
+router.register(r'logoutviewset', LogoutViewSet, basename='logoutviewset')
 router.register(r'shortenedurlviewset', ShortenedURLViewSet, basename='shortenedurlviewset')
+router.register(r'api-access', APIKeyViewSet, basename="api-access")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
